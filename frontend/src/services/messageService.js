473a96c1e6c49d26e338f05ef7bea6149bd1dabe
Gr3st <118692156+Gr3st@users.noreply.github.com>
2024-06-21 01:11:48 +0000

    mess-chat-fetch

--- frontend/src/services/messageService.js
+++ frontend/src/services/messageService.js
@@ -1,28 +1,38 @@
-// messageService.js
-import axios from 'axios';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import EventEmitter from 'events';
+import { privateChatGet } from './chatService';
 
 const messageEventEmitter = new EventEmitter();
 
-export function messageService() {
+export function useMessageService() {
+  const { fetchMessages, fetchChats, chats } = privateChatGet();
   const [senderId, setSenderId] = useState(localStorage.getItem('senderID') || '');
-  const [receiverId, setReceiverId] = useState('');
+  const [receiverId, setReceiverId] = useState(localStorage.getItem('receiverID') || '');
   const [content, setContent] = useState('');
 
-  useEffect(()=>{
-    setReceiverId(localStorage.getItem('receiverID') || '');
-  },[content]);
+  useEffect(() => {
+    fetchChats();
+  }, [fetchChats]);
 
   const handleSendData = async () => {
     const postData = { senderId, receiverId, content };
-    
+
     try {
       const res = await axios.post('https://bookish-adventure-qrv6xv6p4x629x7v-4000.app.github.dev/message', postData);
-      console.log(res);
-      // Clear input content after sending message
-      setContent('');
+      setContent(''); // Clear input content after sending message
+
+      const chat = chats.find(chat =>
+        (chat.user1Id._id === senderId && chat.user2Id._id === receiverId) ||
+        (chat.user2Id._id === senderId && chat.user1Id._id === receiverId)
+      );
+
+      if (chat) {
+        fetchMessages(chat._id);
+      }
+
       messageEventEmitter.emit('messageSent');
+      
     } catch (err) {
       console.error('Error sending message:', err);
     }
@@ -38,4 +48,5 @@ export function messageService() {
     handleSendData,
   };
 }
+
 export { messageEventEmitter };
