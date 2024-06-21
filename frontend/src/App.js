18dcf4c4634eb1ed115c9e7afb4c5b561f2a5ff2
Gr3st <118692156+Gr3st@users.noreply.github.com>
2024-06-21 00:34:07 +0000

    -fixed"/"v2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -64,7 +64,7 @@ function App() {
         </div>
 
         <Routes>
-          <Route path="*" element={<div style={{display: 'none'}}> Not Found or You do not have permission.</div>}/>
+          <Route path="*" element={<></>}/>
           <Route path='/logout' element={<LogOut onLogout={handleLogout} />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/login" element={<LoginForm onLogin={handleLogin}/>} />
